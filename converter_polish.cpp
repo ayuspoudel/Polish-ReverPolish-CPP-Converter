b8c05a95b6b17da8950d3b0a0e0687c5e8ad7106
Ayush Poudel <ayushpoudel@usf.edu>
2024-11-08 13:25:31 -0500

    Error fix

--- converter_polish.cpp
+++ converter_polish.cpp
@@ -3,68 +3,79 @@
 #include <unordered_set>
 #include <stack>
 #include <string>
-#include <cstdlib>
 
+// Define the postfix_converter function
+std::string postfix_converter(const std::string& s) {
+    std::unordered_map<char, int> precedence = {
+        {'+', 1}, 
+        {'-', 1}, 
+        {'*', 2}, 
+        {'/', 2}
+    };
 
-std::string postfix_converter(std::string s){
-        std::unordered_map<char, int> precedence = {{'+', 1}, 
-                                                    {'-', 1}, 
-                                                    {'*', 2},
-                                                    {'/', 2}};
+    std::unordered_map<char, char> opening = {
+        {'(', ')'},
+        {'{', '}'},
+        {'[', ']'}
+    };
 
-        std::unordered_map<char, char> opening = {
-            {'(', ')'},
-            {'{', '}'},
-            {'[', ']'}
-        };
-        std::unordered_map<char, char> closing = {
-            {')', '('},
-            {'}', '{'},
-            {']', '['}
-        };
+    std::unordered_map<char, char> closing = {
+        {')', '('},
+        {'}', '{'},
+        {']', '['}
+    };
 
-        std::unordered_set<char> digit = {'1', '2', '3', '4',
-                                            '5', '6', '7', '8',
-                                            '9', '0'};
-        std::stack<char> st;
-        std::string postfix = "";
-        int j = 0;
-        for(int i = 0;i<s.length(); i++){
-            char curr = s[i];
-            if(opening.count(curr)){ //if opening parenthesis
-                st.push(curr);
+    std::unordered_set<char> digit = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+
+    std::stack<char> st;
+    std::string postfix = "";
+
+    for (char curr : s) {
+        if (opening.count(curr)) {  // If it's an opening parenthesis
+            st.push(curr);
+        }
+        else if (digit.count(curr)) {  // If it's a digit
+            postfix += curr;
+        }
+        else if (precedence.count(curr)) {  // If it's an operator
+            while (!st.empty() && precedence.count(st.top()) && precedence[st.top()] >= precedence[curr]) {
+                postfix += st.top();
+                st.pop();
             }
-            else if(digit.count(curr)){   //if number
-                postfix[j++] = curr;
+            st.push(curr);
+        }
+        else if (closing.count(curr)) {  // If it's a closing parenthesis
+            while (!st.empty() && st.top() != closing[curr]) {
+                postfix += st.top();
+                st.pop();
             }
-            else if(precedence.count(curr)){    //if operator
-                while(!st.empty() && precedence[st.top()]>=precedence[curr]){
-                    postfix[j++] = st.top();
-                    st.pop();
-                }
-                st.push(curr);
-            }
-            else if(closing.count(curr)){
-                while(!st.empty() && st.top() == closing[curr]){
-                    postfix[j++] = st.top();
-                    st.pop();
-                }
-                if(!st.empty()){
-                    if(!opening.count(st.top()))
-                        return "Invalid";
-                    } else{st.pop();}
+            if (!st.empty()) {
+                st.pop();  // Pop the matching opening parenthesis
+            } else {
+                return "Invalid";  // Unmatched closing bracket
             }
         }
-        return postfix;
     }
+
+    // Pop all remaining operators in the stack
+    while (!st.empty()) {
+        if (opening.count(st.top())) {
+            return "Invalid";  // Unmatched opening bracket
+        }
+        postfix += st.top();
+        st.pop();
+    }
+
+    return postfix;
+}
 
 int main() {
     std::string expression;
     std::cout << "Enter an infix expression: ";
     std::cin >> expression;
-    
+
     std::string result = postfix_converter(expression);
     std::cout << "Postfix Expression: " << result << std::endl;
-    
+
     return 0;
 }
